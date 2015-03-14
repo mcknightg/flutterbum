a57b4cae2d92f531c2d91604845bd226b5b188e8
georgemcknight <rsmrallen88@gmail.com>
2015-03-14 05:59:32 -0400

    Fix from Michael Hoeller

--- server/accounts.js
+++ server/accounts.js
@@ -3,5 +3,9 @@ Meteor.startup(function() {
   AccountsEntry.config({
     signupCode: null
   });
-
+  Accounts.onCreateUser( function (options, user) {
+        if (options.profile) user.profile = options.profile;
+        user.username = options.email;
+        return user;
+    });
 });
