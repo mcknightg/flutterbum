7e1c24beec0e17fb7c2b2f8acf2a80a50fb027db
georgemcknight <rsmrallen88@gmail.com>
2015-03-16 22:02:14 -0400

    Add sorting

--- client/views/activity/activity.js
+++ client/views/activity/activity.js
@@ -1,6 +1,25 @@
+Session.setDefault('activitySort',1)
 Template.activity.helpers({
   calevents:function(){
+    var sortorder = Session.get('activitySort');
     return Calevents.find({project:Session.get('active_project')},
-  {reactive:true,sort:{start:'ascending'}})
+  {reactive:true,sort:{start:sortorder}})
+},
+activitySort:function(){
+  var sortorder = Session.get('activitySort');
+  if(sortorder === 1){
+    return 'ASC';
+  } else{
+    return 'DESC';
   }
+}
 })
+Template.activity.events({
+  "click .activitySort": function(event, template){
+     if(Session.get('activitySort') === 1){
+       Session.set('activitySort',-1);
+     } else{
+       Session.set('activitySort',1);
+     }
+  }
+});
